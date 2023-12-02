b6f90cb5749d97506ccc24538dad06d249246e92
Alexis Schultz <Alex293@users.noreply.github.com>
2023-12-02 15:22:51 +0100

    day01

---
+++ Sources/Day01.swift
@@ -0,0 +1,96 @@
+import Algorithms
+import Parsing
+
+struct Day01: AdventDay {
+  // Save your data in a corresponding text file in the `Data` directory.
+  var data: String
+
+  struct Line {
+    var id: Int
+    var draws: [Draw]
+    typealias Draw = [Color: Int]
+  }
+
+  enum Color: String, CaseIterable {
+    case red, green, blue
+  }
+
+  var parsed: [Line] {
+    let draw = Many(into: Line.Draw()) { $0[$1.0] = $1.1 }
+      element: {
+        OneOf {
+          for color in Color.allCases {
+            Parse(input: Substring.self) {
+              " "
+              Int.parser()
+              " \(color.rawValue)"
+            }
+            .map { (color, $0) }
+          }
+        }
+      }
+      separator: {
+        ","
+      }
+
+    let line = Parse(input: Substring.self) {
+      Line(id: $0, draws: $1)
+    } with: {
+      "Game "
+      Int.parser()
+      ":"
+      Many {
+        draw
+      } separator: {
+        ";"
+      }
+    }
+
+    let dataParser = Parse(input: Substring.self) {
+      Many {
+        line
+      } separator: {
+        "\n"
+      } terminator: {
+        OneOf {
+          "\n"
+          ""
+        }
+      }
+    }
+
+    return try! dataParser.parse(data[...])
+  }
+
+  // Replace this with your solution for the first part of the day's challenge.
+  func part1() -> Any {
+    parsed
+      .reduce(into: 0) { sum, line in
+        let isValid = line.draws.allSatisfy {
+            $0[.red, default: 0] <= 12 && $0[.green, default: 0] <= 13 && $0[.blue, default: 0] <= 14
+        }
+        guard isValid else { return }
+        sum += line.id
+      }
+  }
+
+  // Replace this with your solution for the second part of the day's challenge.
+  func part2() -> Any {
+    parsed
+      .reduce(into: 0) { sum, line in
+        let maxs = line.draws
+          .reduce(into: (0,0,0)) { sum, draw in
+            if let red = draw[.red], red > sum.0 {
+              sum.0 = red
+            }
+            if let green = draw[.green], green > sum.1 {
+              sum.1 = green
+            }
+            if let blue = draw[.blue], blue > sum.2 {
+              sum.2 = blue
+            }
+          }
+        sum += maxs.0 * maxs.1 * maxs.2
+      }
+  }
+}
